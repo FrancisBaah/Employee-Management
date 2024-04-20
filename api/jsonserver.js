c97f9006f84acca40376fe9b7c40473b63870c91
FrancisBaah <baahfrancis88@gmail.com>
2024-04-20 13:40:43 +0400

    --

--- api/jsonserver.js
+++ api/jsonserver.js
@@ -1,20 +1,46 @@
-const fs = require("fs");
-const path = require("path");
-
 module.exports = (req, res) => {
-  try {
-    // Read the contents of db.json synchronously
-    const dbFilePath = path.join(__dirname, "db.json");
-    const dbData = fs.readFileSync(dbFilePath, "utf8");
-
-    // Parse the JSON data from db.json
-    const jsonData = JSON.parse(dbData);
-
-    // Respond with the JSON data from db.json
-    res.status(200).json(jsonData);
-  } catch (error) {
-    // Handle errors if reading or parsing fails
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  res.status(200).json({
+    employees: [
+      {
+        name: "Ali",
+        supervisor: "",
+        id: "984b",
+      },
+      {
+        name: "Wafeeq",
+        supervisor: "Ali",
+        id: "f0c3",
+      },
+      {
+        name: "Aysar",
+        supervisor: "Wafeeq",
+        id: "6306",
+      },
+      {
+        name: "Moied",
+        supervisor: "Aysar",
+        id: "2658",
+      },
+      {
+        id: "6a33",
+        name: "Francis Baah",
+        supervisor: "Aysar",
+      },
+      {
+        id: "06ff",
+        name: "Charlot",
+        supervisor: "Ali",
+      },
+      {
+        id: "6718",
+        name: "regg",
+        supervisor: "Aysar",
+      },
+      {
+        id: "9028",
+        name: "bbf",
+        supervisor: "Aysar",
+      },
+    ],
+  });
 };
